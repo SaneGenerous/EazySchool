55150506b5a0b60247f95342aa303fe6926362fc
Sane Generous <karim2k11@gmail.com>
2025-07-31 18:22:30 +0100

    Contact Validation form implemented.

--- src/main/java/td/msk/eazyschool/controllers/ContactController.java
+++ src/main/java/td/msk/eazyschool/controllers/ContactController.java
@@ -1,8 +1,12 @@
 package td.msk.eazyschool.controllers;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,14 +29,18 @@ public class ContactController {
 
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @PostMapping(value = "/saveMsg")
-    public ModelAndView saveMessage(Contact contact) {
-        log.info("Name: {}", contact.getName());
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if(errors.hasErrors()) {
+            log.error("Contact form validation failed due to: {}", errors.getAllErrors().toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
