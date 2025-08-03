1bb4d32949bfd84b94cf381eb49b18d4c5cbd192
Sane Generous <karim2k11@gmail.com>
2025-08-03 21:22:39 +0100

    Creating login and dashboard pages and implement security with Thymeleaf.

--- src/main/java/td/msk/eazyschool/config/ProjectSecurityConfig.java
+++ src/main/java/td/msk/eazyschool/config/ProjectSecurityConfig.java
@@ -6,7 +6,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,14 +19,33 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests.requestMatchers("/", "/home").permitAll()
+                     .requestMatchers("/dashboard").authenticated()
                      .requestMatchers("/holidays/**").permitAll()
                      .requestMatchers("/contact").permitAll()
                      .requestMatchers("/saveMsg").permitAll()
                      .requestMatchers("/courses").permitAll()
                      .requestMatchers("/about").permitAll()
+                     .requestMatchers("/login").permitAll()
                      .requestMatchers("/assets/**").permitAll())
-                     .formLogin(Customizer.withDefaults())
+                     .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                     .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+                     .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+                     .invalidateHttpSession(true).permitAll())
                      .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
